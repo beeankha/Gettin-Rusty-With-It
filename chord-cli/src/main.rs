b5e31d3a4733170cb22b9a862fd163eb373212c5
Bianca Henderson <beeankha@gmail.com>
2022-10-03 21:35:43 -0400

    Add more chords, update README

--- chord-cli/src/main.rs
+++ chord-cli/src/main.rs
@@ -1,5 +1,10 @@
+use clap::Parser;
+use std::collections::HashMap;
+
 const FRETBOARD: &str = "◯ ◯ ◯ ◯ ◯ ◯
-┌─┬─┬─┬─┬─┐
+╒═╤═╤═╤═╤═╕
+│ │ │ │ │ │
+├─┼─┼─┼─┼─┤
 │ │ │ │ │ │
 ├─┼─┼─┼─┼─┤
 │ │ │ │ │ │
@@ -7,9 +12,7 @@ const FRETBOARD: &str = "◯ ◯ ◯ ◯ ◯ ◯
 │ │ │ │ │ │
 └─┴─┴─┴─┴─┘";
 
-use clap::Parser;
-
-/// A CLI tool that displays guitar chords
+/// A CLI to show you how to play a guitar chord
 #[derive(Parser, Debug)]
 #[clap(version, about)]
 struct Args {
@@ -21,5 +24,33 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    println!("\nThis is how you play the '{}' chord: \n\n{}\n", args.name, FRETBOARD);
+    let chords: HashMap<&str, &str> =
+        HashMap::from([
+            ("C", "x32010"),
+            ("G", "320003"),
+            ("D", "xx0232"),
+            ("Eb", "xx1343")
+            ]);
+
+    match chords.get(&args.name[..]) {
+        None => println!("Unknown chord '{}'", args.name),
+        Some(pattern) => {
+            let mut board: Vec<char> = FRETBOARD.chars().collect();
+            for (i, ch) in pattern.chars().enumerate() {
+                let idx: usize = i * 2;
+                if ch == 'x' {
+                    board[idx] = ch
+                } else {
+                    let value: usize = ch.to_digit(10).unwrap() as usize;
+                    board[idx] = ' ';
+                    board[idx + 24 * value] = '◯'
+                }
+            }
+            println!(
+                "\nThis is how you play the '{}' chord: \n\n{}\n",
+                args.name,
+                board.iter().collect::<String>()
+            )
+        }
+    }
 }
